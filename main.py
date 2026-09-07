d9dd2ba072342b49e37f3abf103164632462ea6b
SaVok-gybe173 <gybe173@gmail.com>
2026-09-07 21:52:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,24 @@ from rich.highlighter import Highlighter
 from rich.console import Console
 from termcolor import cprint
 from typing import TypedDict
+from PIL import Image
 
-import re
 import win32gui
+import win32ui
+import win32con
 import win32process
-
+import pyautogui
+import re
+import time
+import ctypes
+try:
+    ctypes.windll.user32.SetProcessDPIAware()
+except:
+    pass   # для старых Windows можно использовать SetProcessDPIAwareness
 class TypedDictInfoProcess(TypedDict):
     title: str
     hwnd: int
+    _class: str
     pid: int
 
     server: str
@@ -23,6 +33,7 @@ class MoneyHighlighter(Highlighter):
            text.stylize("bold blue", match.start(), match.end())
        for match in re.finditer(r'\\', text.plain):
            text.stylize("bold blue", match.start(), match.end())
+
 
 RED = "\033[31m"
 GREEN = "\033[32m"
@@ -53,7 +64,7 @@ def get_detailed_windows() -> list[TypedDictInfoProcess]:
                 windows.append({
                     'hwnd': hwnd,
                     'title': title,
-                    'class': class_name,
+                    '_class': class_name,
                     'pid': pid
                 })
 
@@ -71,10 +82,10 @@ def updateUsers():
         title: str = win['title'].replace(' ', '')
         if len(mass := title.split('|')) == 3 and mass[0] == "GribLand":
             print(f"{GREEN}[+]{RESET} Найден игрок {mass[-1]} на сервере {mass[-2]}, HWND:{win['hwnd']}")
-            users.append({"hwnd": win['hwnd'], "server": mass[-2], "user": mass[-1], "title": win['title'], "pid": win["pid"]})
+            users.append({"hwnd": win['hwnd'], "server": mass[-2], "user": mass[-1], "title": win['title'], "pid": win["pid"], "_class": win['_class']})
             
     if len(users) == 0:
-        print("{RED}[-]{RESET} Не найден ни одино активное окно")
+        print(f"{RED}[-]{RESET} Не найден ни одино активное окно")
     elif len(users) == 1:
         print()
         user = 0
@@ -98,3 +109,41 @@ def updateUsers():
 
 updateUsers()
 
+def turnaround(hwnd) -> None:
+    # Если окно свёрнуто — разворачиваем
+    if win32gui.IsIconic(hwnd):
+        print("Окно свёрнуто, разворачиваю...")
+        win32gui.ShowWindow(hwnd, win32con.SW_RESTORE)   # SW_RESTORE = 9
+        win32gui.SetForegroundWindow(hwnd)
+        time.sleep(0.3)  # даём время на отрисовку (можно увеличить до 0.5)
+        return True
+    return False
+
+def minimize_back(hwnd):
+    """Сворачивает окно."""
+    win32gui.ShowWindow(hwnd, win32con.SW_MINIMIZE)
+
+def capture_window_printwindow(hwnd):
+    rect = win32gui.GetClientRect(hwnd)
+    left, top = win32gui.ClientToScreen(hwnd, (rect[0], rect[1]))
+    right, bottom = win32gui.ClientToScreen(hwnd, (rect[2], rect[3]))
+    width = right - left
+    height = bottom - top
+    return pyautogui.screenshot(region=(left, top, width, height))
+
+
+def scrin():
+    global users, user
+    if not users:
+        return
+    hwnd = win32gui.FindWindow(users[0]['_class'], users[0]['title'])
+    if hwnd == 0:
+        print(f"{RED}[-]{RESET} Окно не найдено")
+        updateUsers()
+    else:
+        _is = turnaround(hwnd)
+        img = capture_window_printwindow(hwnd)
+        img.save("screenshot_window.png")
+        if _is:
+            minimize_back(hwnd)
+scrin()
